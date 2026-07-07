e30239ab7f5de60accd44fe7bab4974fcb302d5c
alicaldini <ali.caldini@gmail.com>
2026-07-07 16:26:42 +0200

    correzioni codice

--- events/views.py
+++ events/views.py
@@ -11,10 +11,6 @@ class EventListView(ListView):
     context_object_name = 'events'
     ordering = ['date']
 
-def event_list(request):
-    events = Event.objects.all().order_by('date')
-    return render(request, 'events/event_list.html', {'events': events})
-
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
     already_registered = False
